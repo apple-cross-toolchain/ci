edea9b111ddfbfe1c058b86d091d985c9278e2d4
Thi Doãn <t@thi.im>
2026-03-02 11:52:33 +0000

    Strip debug symbols and clean up leftover dirs in strip scripts
    - Run `strip` on binaries to remove debug symbols (Swift: 62M -> 14M,
      Clang: 61M -> 56M)
    - Remove leftover `local/` directory from both tarballs
    - Fix strip-swift.sh to move actual binaries (swift-driver,
      swift-package) instead of dangling symlinks

--- tools/strip-clang.sh
+++ tools/strip-clang.sh
@@ -18,8 +18,9 @@ mv \
   bin/lld \
   bin/llvm-nm \
   bin-new
-rm -rf bin lib libexec include share
+rm -rf bin lib libexec include share local
 mv bin-new bin
+strip bin/clang-17 bin/lld bin/llvm-nm
 popd
 tar -Jcf clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04-stripped.tar.xz clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04
 sha256sum clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04-stripped.tar.xz > clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04-stripped.tar.xz.sha256
--- tools/strip-swift.sh
+++ tools/strip-swift.sh
@@ -9,13 +9,13 @@ curl -LO https://download.swift.org/swift-6.2.3-release/ubuntu2204/swift-6.2.3-R
 tar -xf swift-6.2.3-RELEASE-ubuntu22.04.tar.gz
 pushd swift-6.2.3-RELEASE-ubuntu22.04/usr
 mkdir bin-new
-mv \
-  bin/swift \
-  bin/swiftc \
-  bin/swift-build \
-  bin-new
-rm -rf bin lib libexec include share
+mv bin/swift-driver bin/swift-package bin-new
+ln -s swift-driver bin-new/swift
+ln -s swift-driver bin-new/swiftc
+ln -s swift-package bin-new/swift-build
+rm -rf bin lib libexec include share local
 mv bin-new bin
+strip bin/swift-driver bin/swift-package
 popd
 tar -Jcf swift-6.2.3-RELEASE-ubuntu22.04-stripped.tar.xz swift-6.2.3-RELEASE-ubuntu22.04
 sha256sum swift-6.2.3-RELEASE-ubuntu22.04-stripped.tar.xz > swift-6.2.3-RELEASE-ubuntu22.04-stripped.tar.xz.sha256
